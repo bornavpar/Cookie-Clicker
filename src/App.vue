a6aae12ce31a38750694926503abf52bf02480f7
borpar <borjanavarropardo@gmail.com>
2023-11-10 09:51:14 +0100

    Version 0.1.1

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,12 @@
 
   </div>
   <center>
-    <button class="galleta" @click="puntuarConClick">Galletita</button>
+    <button class="galleta" @click="puntuarConClick"></button>
     <p></p>
     <div class="botones">
-      <button :disabled="!checkBoton1" @click="primeraCompra">Primera mejora</button>
-      <button :disabled="!checkBoton2" @click="segundaCompra">Segunda mejora</button>
-      <button :disabled="!checkBoton3" @click="terceraCompra">Tercera mejora</button>
+      <button :disabled="!checkBoton1" @click="primeraCompra">Primera mejora: {{ boton1Precio }}$</button>
+      <button :disabled="!checkBoton2" @click="segundaCompra">Segunda mejora: {{ boton2Precio }}$</button>
+      <button :disabled="!checkBoton3" @click="terceraCompra">Tercera mejora: {{ boton3Precio }}$</button>
     </div>
   </center>
 </template>
@@ -24,6 +24,9 @@ export default {
     return {
       puntuacion: 0,
       puntuacionPasiva: 0,
+      boton1Precio: 10,
+      boton2Precio: 25,
+      boton3Precio: 50,
       checkBoton1: false,
       checkBoton2: false,
       checkBoton3: false
@@ -33,14 +36,19 @@ export default {
     primeraCompra() {
       this.puntuacion -= 10
       this.puntuacionPasiva++
+      this.boton1Precio = parseInt(this.boton1Precio * 1.1)
     },
     segundaCompra() {
       this.puntuacion -= 25
       this.puntuacionPasiva = this.puntuacionPasiva + 5
+      this.boton2Precio = parseInt(this.boton2Precio * 1.1)
+
     },
     terceraCompra() {
       this.puntuacion -= 50
       this.puntuacionPasiva = this.puntuacionPasiva + 10
+      this.boton3Precio = parseInt(this.boton3Precio * 1.1)
+
     },
     puntuarConClick() {
       this.puntuacion++
@@ -77,9 +85,13 @@ export default {
 
 <style>
 .galleta {
-  height: 100px;
-  width: 100px;
-  border-radius: 50%;
+  background-image: url('./assets/galletita.png');
+  background-repeat: no-repeat;
+  background-size: contain;
+  height: 300px;
+  width: 300px;
+  border: none;
+  cursor:pointer;
 }
 
 .cabecera {
@@ -87,7 +99,6 @@ export default {
 }
 
 .botones {
-
   display: inline;
 }
 </style>
